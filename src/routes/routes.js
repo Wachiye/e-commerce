00b8947dac600fdf872375339a09862b4728bc5e
Wachiye Siranjofu <47672963+Wachiye@users.noreply.github.com>
2021-02-24 17:16:13 +0300

    Added blog and cart routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -14,7 +14,12 @@ import HomePage from "../pages/HomePage";
 import AboutPage from "../pages/AboutPage";
 import ProductsPage from "../pages/ProductsPage";
 import ProductPage from "../pages/ProductPage";
-const routes = () => {
+import BlogPage from "../pages/BlogPage";
+import PostPage from "../pages/PostPage";
+import CartPage from "../pages/CartPage";
+import ContactPage from "../pages/ContactPage";
+
+const Routes = () => {
   return (
     <Router>
       {/* //define paths and routes */}
@@ -43,43 +48,33 @@ const routes = () => {
           component={ProductPage}
           layout={PublicLayout}
         />
+        <PublicRoute
+          path="/blog"
+          exact
+          component={BlogPage}
+          layout={PublicLayout}
+        />
+        <PublicRoute
+          path="/blog/:slug"
+          exact
+          component={PostPage}
+          layout={PublicLayout}
+        />
+        <PublicRoute
+          path="/contact"
+          exact
+          component={ContactPage}
+          layout={PublicLayout}
+        />
+        <PublicRoute
+          path="/cart"
+          exact
+          component={CartPage}
+          layout={PublicLayout}
+        />
       </Switch>
     </Router>
   );
 };
-export default routes;
-import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-
-import PublicLayout from '../layouts/PublicLayout';
-// import PrivateLayout from '../layouts/PrivateLayout';
-
-// import PrivateRoute from './PrivateRoute';
-import PublicRoute from './PublicRoute';
-
-import HomePage from '../pages/HomePage';
-import AboutPage from '../pages/AboutPage';
-import ProductsPage from '../pages/ProductsPage';
-import ProductPage from '../pages/ProductsPage';
-import ContactPage from '../pages/ContactPage';
-import BlogPage from '../pages/BlogPage';
-import PostPage from '../pages/PostPage';
-
-const Routes = () => {
-  return(
-    <Router>
-      <Switch>
-        <PublicRoute path="/" exact component={HomePage} layout={PublicLayout} />
-        <PublicRoute path="/about" exact component={AboutPage} layout={PublicLayout} />
-        <PublicRoute path="/products" exact component={ProductsPage} layout={PublicLayout} />
-        <PublicRoute path="/products/:slug" exact component={ProductPage} layout={PublicLayout} />
-        <PublicRoute path="/products/categories/:name" exact component={ProductsPage} layout={PublicLayout} />
-        <PublicRoute path="/contact" exact component={ContactPage} layout={PublicLayout} />
-        <PublicRoute path="/blog" exact component={BlogPage} layout={PublicLayout} />
-        <PublicRoute path="/blog/:slug" exact component={PostPage} layout={PublicLayout} />
-      </Switch>
-    </Router>
-  )
-}
 
 export default Routes;
